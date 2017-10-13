9a508dc951cae032f96402ac22bcceeb441ff6bd
Jeff1741 <jeffrey.jev@gmail.com>
2017-10-13 15:44:41 -0400

    read serial data, transmit to GS

--- SystemEng_V1/Radios.ino
+++ SystemEng_V1/Radios.ino
@@ -1,9 +1,22 @@
 void getRadioData(){
   //Gets Data from the serial
+   if(Serial.available()){
+    receiveSerialData();
+  }
 }
 
 void sendRadioData(){
   //Sends location & panic button state
+  Serial1.print(telemetry[teleCount]);
+  Serial1.print(',');
+  Serial1.print(telemetry[teleButton]);
+  Serial1.print(',');
+  Serial1.print(telemetry[teleIMUx]);
+  Serial1.print(',');
+  Serial1.print(telemetry[teleIMUy]);
+  Serial1.print(',');
+  Serial1.print(telemetry[teleIMUz]);
+  Serial1.print(',');
 }
 
 
