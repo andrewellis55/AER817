18e92247d3674435fd388cc97e6f88293a0ab253
Jeff1741 <jeffrey.jev@gmail.com>
2017-10-20 21:12:26 -0400

    Radio now transmits data, button check added

--- SystemEng_V1/SystemEng_V1.ino
+++ SystemEng_V1/SystemEng_V1.ino
@@ -1,16 +1,11 @@
 //Libraries
 
-
-//Debug State Function
-#ifdef DEBUG
-#define Debug(x); Serial.println(x);
-#else
-#define Debug(x); 
-#endif
+#define DEBUG
 
 //Global Variable Declarations
 int delayTime;
-int dataSize = 5;
+int buttonPin;
+int buttonState;
 
 //Telemetry packet
 #define teleCount 0
@@ -18,19 +13,27 @@ int dataSize = 5;
 #define teleIMUx 2
 #define teleIMUy 3
 #define teleIMUz 4
-float telemetry[dataSize];
+float telemetry[5];
 
 void setup() {
+
+  Serial1.begin(9600);
+
+  //Debug State Function
+  #ifdef DEBUG
+    Serial.begin(9600);
+  #endif
+
   // put your setup code here, to run once:
   initGlobalVariables();
+  pinMode(buttonPin, INPUT);
   initIMU();
   setLocationReference();
-  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+
   updateLocation();
   checkPanicButton();
   getRadioData();
@@ -38,9 +41,8 @@ void loop() {
   timeDelay();
 
   telemetry[teleCount] = telemetry[teleCount] + 1; //increment the packet counter
-  
-}
 
+}
 
 
 
