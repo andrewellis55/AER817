96c31d59ee50f74257a9e16fb95c47864be8d3dd
Jeff1741 <jeffrey.jev@gmail.com>
2017-10-20 21:11:20 -0400

    added panic button checking

--- SystemEng_V1/Sensors.ino
+++ SystemEng_V1/Sensors.ino
@@ -5,5 +5,12 @@ void initIMU(){
 
 void checkPanicButton(){
   //Checks if panic button has been pressed
-}
+  buttonState = digitalRead(buttonPin);
 
+  if (buttonState == HIGH){
+    telemetry[teleButton] = 0;
+  }
+  else if (buttonState == LOW){
+    telemetry[teleButton] = 1;
+  }
+}
