eb4f1439ad968f25fad5e954ca05b48caad31ec0
Jeff1741 <jeffrey.jev@gmail.com>
2017-11-14 14:36:35 -0500

    IMU init, placeholder GPS and BMP data added

--- SystemEng_V1/Sensors.ino
+++ SystemEng_V1/Sensors.ino
@@ -1,7 +1,28 @@
 void initIMU(){
   //initialization code for IMU
-}
+    Wire.begin();
 
+    // Calibrate gyro and accelerometers, load biases in bias registers
+    myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
+
+    myIMU.initMPU9250();
+    // Initialize device for active mode read of acclerometer, gyroscope, and
+    // temperature
+
+    // Get magnetometer calibration from AK8963 ROM
+    myIMU.initAK8963(myIMU.factoryMagCalibration);
+    // Initialize device for active mode read of magnetometer
+
+    // Get sensor resolutions, only need to do this once
+    myIMU.getAres();
+    myIMU.getGres();
+    myIMU.getMres();
+
+    // The next call delays for 4 seconds, and then records about 15 seconds of
+    // data to calculate bias and scale.
+    myIMU.magCalMPU9250(myIMU.magBias, myIMU.magScale);
+    delay(2000); // Add delay to see results before serial spew of data
+}
 
 void checkPanicButton(){
   //Checks if panic button has been pressed
@@ -13,4 +34,15 @@ void checkPanicButton(){
   else if (buttonState == LOW){
     telemetry[teleButton] = 1;
   }
+}
+
+
+void getGPSData(){
+  telemetry[teleGPSLat] = 0;
+  telemetry[teleGPSLong] = 0;
+  telemetry[teleGPSAlt] = 1;
+}
+
+void getBMPData(){
+  telemetry[teleBMPAlt] = 0;
 }
