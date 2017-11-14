d6d91c668f2c1d39110373eb404cd4ff5d7d90bf
wiltravis <32307963+wiltravis@users.noreply.github.com>
2017-11-14 14:16:17 -0500

    Update Update_EEG.m

--- MatLab/GS2017/Update_EEG.m
+++ MatLab/GS2017/Update_EEG.m
@@ -10,6 +10,31 @@ function [] = Update_EEG(handles)
 %
 %% Main Code
 
+% After reading in 2 seconds of sensor data
+avg_sens1 = mean([IXDATA.raw.eeg.data(:,1)]);
+avg_sens4 = mean([IXDATA.raw.eeg.data(:,4)]);
+time = linspace(0,2,length(IXDATA.raw.eeg.data(:,1)));
+sensor1 = IXDATA.raw.eeg.data(:,1);
+sensor4 = IXDATA.raw.eeg.data(:,4);
+
+% Blink check
+n=1;
+blink = 0;
+while (n<=457)
+
+% Sensor 1 blink check
+if (sensor1(n) > avg_sens1*1.20 || sensor1(n) < avg_sens1*0.5)
+    blink = 1;
+end
+
+% Sensor 4 blink check
+if (sensor4(n) > avg_sens4*1.20 || sensor4(n) < avg_sens4*0.5)
+    blink = 1;
+end
+
+n = n+1;
+end
+
 
 
 
