2653e081dacdd48e7494c56558346508afba29c1
Jeff1741 <jeffrey.jev@gmail.com>
2017-11-14 14:36:07 -0500

    IMU data added (not tested), still needs SLAM

--- SystemEng_V1/SLAM.ino
+++ SystemEng_V1/SLAM.ino
@@ -4,5 +4,19 @@ void setLocationReference(){
 
 void updateLocation(){
   //Updates location based on IMU data using SLAM algorythem
-}
+   if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+    {
+      myIMU.readAccelData(myIMU.accelCount);  // Read the x/y/z adc values
+  
+//      Now we'll calculate the accleration value into actual g's
+//      This depends on scale being set
+//      myIMU.ax = (float)myIMU.accelCount[0] * myIMU.aRes; // - myIMU.accelBias[0];
+//      myIMU.ay = (float)myIMU.accelCount[1] * myIMU.aRes; // - myIMU.accelBias[1];
+//      myIMU.az = (float)myIMU.accelCount[2] * myIMU.aRes; // - myIMU.accelBias[2];
+     } 
+  
+    telemetry[teleIMULat] = (float)myIMU.accelCount[0];
+    telemetry[teleIMULong] = (float)myIMU.accelCount[1];
+    telemetry[teleIMUAlt] = (float)myIMU.accelCount[2];
 
+}
