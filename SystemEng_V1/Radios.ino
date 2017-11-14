a5f903555b8e433a24789439dd1aac77a52f6b5c
Jeff1741 <jeffrey.jev@gmail.com>
2017-11-14 14:35:31 -0500

    Update Radios.ino

--- SystemEng_V1/Radios.ino
+++ SystemEng_V1/Radios.ino
@@ -6,28 +6,23 @@ void getRadioData(){
 }
 
 void sendRadioData() {
-  //Sends location & panic button state
-  Serial1.print(telemetry[teleCount]);
-  Serial1.print(',');
-  Serial1.print(telemetry[teleButton]);
-  Serial1.print(',');
-  Serial1.print(telemetry[teleIMUx]);
-  Serial1.print(',');
-  Serial1.print(telemetry[teleIMUy]);
-  Serial1.print(',');
-  Serial1.print(telemetry[teleIMUz]);
-  Serial1.println(',');
+  //Sends data to GS
 
+  for(int i = 0; i < teleSize; i++)
+  {
+    Serial1.print(telemetry[i]);
+    Serial1.print(',');
+  }
+  
+  Serial1.println(' ');
 
-  //DEBUG STUFF
-  Serial.print(telemetry[teleCount]);
-  Serial.print(',');
-  Serial.print(telemetry[teleButton]);
-  Serial.print(',');
-  Serial.print(telemetry[teleIMUx]);
-  Serial.print(',');
-  Serial.print(telemetry[teleIMUy]);
-  Serial.print(',');
-  Serial.print(telemetry[teleIMUz]);
-  Serial.println(',');
+  //DEBUG OUTPUT
+  for(int i = 0; i < teleSize; i++)
+  {
+    Serial.print(telemetry[i]);
+    Serial.print(',');
+  }
+  
+  Serial.println(' ');
+
 }
