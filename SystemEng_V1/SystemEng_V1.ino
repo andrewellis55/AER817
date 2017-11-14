4b95d8683dd9fc3d140282573a772c92249b2951
Jeff1741 <jeffrey.jev@gmail.com>
2017-11-14 14:37:43 -0500

    Updated packet definition, added device toggle
    Can switch between PERSON and PAYLOAD

--- SystemEng_V1/SystemEng_V1.ino
+++ SystemEng_V1/SystemEng_V1.ino
@@ -2,21 +2,53 @@
 
 #define DEBUG
 
+#define PAYLOAD
+
 //Global Variable Declarations
 int delayTime;
 int buttonPin;
 int buttonState;
+int DeviceID = 1234;
 
 //Telemetry packet
-#define teleCount 0
-#define teleButton 1
-#define teleIMUx 2
-#define teleIMUy 3
-#define teleIMUz 4
-float telemetry[5];
+#ifdef PERSON
+  #define teleDeviceID 0
+  #define teleDevice 1
+  #define teleCount 2
+  #define teleButton 3
+  #define teleBMPAlt 4
+  #define teleIMULat 5
+  #define teleIMULong 6
+  #define teleIMUAlt 7
+  #define teleGPSLat 8
+  #define teleGPSLong 9
+  #define teleGPSAlt 10
+  #define teleSize 11
+  float telemetry[11];
+#endif
+
+#ifdef PAYLOAD
+  #define teleDeviceID 0
+  #define teleDevice 1
+  #define teleCount 2
+  #define teleButton 3
+  #define teleBMPAlt 4
+  #define teleIMULat 5
+  #define teleIMULong 6
+  #define teleIMUAlt 7
+  #define teleGPSLat 8
+  #define teleGPSLong 9
+  #define teleGPSAlt 10
+  #define teleHPFront 11
+  #define teleHPRight 12
+  #define teleHPLeft 13
+  #define teleHPBack 14
+  #define teleSize 15
+  float telemetry[15];
+#endif
 
 void setup() {
-
+  
   Serial1.begin(9600);
 
   //Debug State Function
@@ -24,6 +56,18 @@ void setup() {
     Serial.begin(9600);
   #endif
 
+  #ifdef PERSON
+    teleDevice == "PERSON";
+  #endif
+   #ifdef PAYLOAD
+    teleDevice == "PAYLOAD";
+  #endif
+
+  telemetry[teleDeviceID] = DeviceID;
+  telemetry[teleDevice] = teleDevice;
+
+  MPU9250 myIMU;
+  
   // put your setup code here, to run once:
   initGlobalVariables();
   pinMode(buttonPin, INPUT);
@@ -36,14 +80,15 @@ void loop() {
 
   updateLocation();
   checkPanicButton();
-  getRadioData();
+//  getRadioData();
+  getGPSData();
+  getBMPData();
   sendRadioData();
   timeDelay();
 
   telemetry[teleCount] = telemetry[teleCount] + 1; //increment the packet counter
 
 }
-
 
 
 
