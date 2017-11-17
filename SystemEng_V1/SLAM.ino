71d39b6a8c2542785b7fb7c61e77827dc12a5ae7
Jeff1741 <jeffrey.jev@gmail.com>
2017-11-16 20:06:37 -0500

    preliminary SLAM (most likely wrong)

--- SystemEng_V1/SLAM.ino
+++ SystemEng_V1/SLAM.ino
@@ -1,22 +1,61 @@
-void setLocationReference(){
+void setLocationReference() {
   //Sets the current location to (0,0,0)
-}
-
-void updateLocation(){
-  //Updates location based on IMU data using SLAM algorythem
-   if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
-    {
-      myIMU.readAccelData(myIMU.accelCount);  // Read the x/y/z adc values
-  
-//      Now we'll calculate the accleration value into actual g's
-//      This depends on scale being set
-//      myIMU.ax = (float)myIMU.accelCount[0] * myIMU.aRes; // - myIMU.accelBias[0];
-//      myIMU.ay = (float)myIMU.accelCount[1] * myIMU.aRes; // - myIMU.accelBias[1];
-//      myIMU.az = (float)myIMU.accelCount[2] * myIMU.aRes; // - myIMU.accelBias[2];
-     } 
-  
-    telemetry[teleIMULat] = (float)myIMU.accelCount[0];
-    telemetry[teleIMULong] = (float)myIMU.accelCount[1];
-    telemetry[teleIMUAlt] = (float)myIMU.accelCount[2];
 
 }
+
+void updateLocation() {
+  //
+
+  //Gets first acceleration data from IMU
+  if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+  {
+    myIMU.readAccelData(myIMU.accelCount);  // Read the x/y/z adc values
+    myIMU.ax = (float)myIMU.accelCount[0] * myIMU.aRes * g; // - myIMU.accelBias[0];
+    myIMU.ay = (float)myIMU.accelCount[1] * myIMU.aRes * g; // - myIMU.accelBias[1];
+    myIMU.az = (float)myIMU.accelCount[2] * myIMU.aRes * g - g; // - myIMU.accelBias[2];
+  }
+
+  imuTimeDelay(); // delays 100 ms
+
+  //  //Gets second reading of acceleration form IMU
+  //  if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+  //  {
+  //    myIMU.readAccelData(myIMU.accelCount);  // Read the x/y/z adc values
+  //    myIMU2.ax = (float)myIMU.accelCount[0] * myIMU.aRes * g; // - myIMU.accelBias[0];
+  //    myIMU2.ay = (float)myIMU.accelCount[1] * myIMU.aRes * g; // - myIMU.accelBias[1];
+  //    myIMU2.az = (float)myIMU.accelCount[2] * myIMU.aRes * g - g; // - myIMU.accelBias[2];
+  //  }
+
+#ifdef DEBUG_IMU //Outputs raw acceleration data
+  telemetry[teleIMUx] = myIMU.ax;
+  telemetry[teleIMUy] = myIMU.ay;
+  telemetry[teleIMUz] = myIMU.az;
+
+#else //Outputs "distance" values from acceleration data
+// IMU X DIRECTION
+  if (myIMU.ax * 0.05 <= 0.05){
+    telemetry[teleIMUx] = 0;
+  }
+  else {
+    telemetry[teleIMUx] = myIMU.ax * 0.05;
+  }
+
+// IMU Y DIRECTION
+  if (myIMU.ax * 0.05 <= 0.05){
+    telemetry[teleIMUy] = 0;
+  }
+  else {
+    telemetry[teleIMUy] = myIMU.ay * 0.05;
+  }
+  
+// IMU Z DIRECTION
+  if (myIMU.ax * 0.05 <= 0.05){
+    telemetry[teleIMUz] = 0;
+  }
+  else {
+    telemetry[teleIMUz] = myIMU.az * 0.05;
+  }
+#endif
+
+}
+
