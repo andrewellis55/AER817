274666e0142b0896eaeb4cb279c932c81da3802c
Jeff1741 <jeffrey.jev@gmail.com>
2017-11-16 20:07:26 -0500

    Update functions.ino

--- SystemEng_V1/functions.ino
+++ SystemEng_V1/functions.ino
@@ -1,10 +1,18 @@
-void initGlobalVariables(){
+void initGlobalVariables() {
   //keeps global variable initializations in one place
   delayTime = 1000; //milliseconds
+  g = 9.81; // m/s/s
 }
 
-void timeDelay(){
+void timeDelay() {
   //Delay time so that data transmission only occurs once per second
-    unsigned long currentTime = millis();
-    while(millis()-currentTime < 1000){}
+  unsigned long currentTime = millis();
+  while (millis() - currentTime < 1000) {}
 }
+
+void imuTimeDelay() {
+  //Delay time so that data transmission only occurs once per second
+  unsigned long currentTime = millis();
+  while (millis() - currentTime < 100) {}
+}
+
